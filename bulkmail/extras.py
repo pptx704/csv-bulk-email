3357d7c66923ebea6ea38c8ad62a83e657706b4f
roughweed <cavnedi@gmail.com>
2020-12-26 19:21:05 +0600

    Reformatted 'version 1.0.2'

--- bulkmail/extras.py
+++ bulkmail/extras.py
@@ -1,25 +1,27 @@
 import re
 from datetime import datetime
 
-#function for parsing templates
-def replace_var(text:str, variables:dict, row:dict):
+# function for parsing templates
+def replace_var(text: str, variables: dict, row: dict):
     text = text
     for i in variables.keys():
-        text = re.sub(f'<{i}>', row[variables[i]], text)
+        text = re.sub(f"<{i}>", row[variables[i]], text)
 
     return text
 
-#string format of current time
+
+# string format of current time
 def now():
     return datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
 
-#default variables for gmail env
+
+# default variables for gmail env
 gmailvars = """\nSMTP_HOST='smtp.gmail.com'
 SMTP_PORT='587'
 SENDER_EMAIL='youremail@gmail.com'
 SENDER_PASSWORD='yourpassword'
 """
-#template message
+# template message
 template_message = """Dear <var1>,
 
 This is a test message. Please change this template according to your will before sending your <var2> mails.
--- bulkmail/mailfunctions.py
+++ bulkmail/mailfunctions.py
@@ -8,15 +8,15 @@ from email.mime.text import MIMEText
 
 from .extras import now, replace_var
 
-#start new session
+# start new session
 def startsession(credentials):
-    #loading credentials
+    # loading credentials
     SMTP_HOST = credentials.get("SMTP_HOST")
     SMTP_PORT = credentials.get("SMTP_PORT")
     SENDER_EMAIL = credentials.get("SENDER_EMAIL")
     SENDER_PASSWORD = credentials.get("SENDER_PASSWORD")
-    
-    #creating smtp session
+
+    # creating smtp session
     session = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
     session.ehlo_or_helo_if_needed()
     session.starttls()
@@ -25,37 +25,46 @@ def startsession(credentials):
     return session
 
 
-#for sending bulk email where everybody will get same email
-def sendnovar(database:str, body:str, subject:str, email_field:str, credentials:dict, type:str):
-    #loading database
-    database = open(database, 'r', encoding='utf-8')
+# for sending bulk email where everybody will get same email
+def sendnovar(
+    database: str,
+    body: str,
+    subject: str,
+    email_field: str,
+    credentials: dict,
+    type: str,
+):
+    # loading database
+    database = open(database, "r", encoding="utf-8")
     reader = DictReader(database)
 
-    #start 
+    # start
     session = startsession(credentials)
     SENDER_EMAIL = credentials.get("SENDER_EMAIL")
 
-    #making logfiles
-    successlog = open('success.log', 'a', encoding='utf-8')
-    failurelog = open('failure.log', 'a', encoding='utf-8')
+    # making logfiles
+    successlog = open("success.log", "a", encoding="utf-8")
+    failurelog = open("failure.log", "a", encoding="utf-8")
     successlog.write(f"\nSession starts at {now()}\n")
     failurelog.write(f"\nSession starts at {now()}\n")
-    
-    #email processing
+
+    # email processing
     for row in reader:
         RECEIVER_EMAIL = row[email_field]
         try:
             email = MIMEMultipart()
-            email['From'] = SENDER_EMAIL
-            email['To'] = RECEIVER_EMAIL
-            email['Subject'] = subject
+            email["From"] = SENDER_EMAIL
+            email["To"] = RECEIVER_EMAIL
+            email["Subject"] = subject
             email.attach(MIMEText(body, type))
             session.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, email.as_string())
             successlog.write(f"Email sent to {RECEIVER_EMAIL} at {now()}\n")
         except:
-            failurelog.write(f"Failed to send email to {RECEIVER_EMAIL}. Time: {now()}\n")
-    
-    #ending session and closing logs
+            failurelog.write(
+                f"Failed to send email to {RECEIVER_EMAIL}. Time: {now()}\n"
+            )
+
+    # ending session and closing logs
     successlog.write(f"\nSession ends at {now()}\n-----------------------\n")
     failurelog.write(f"\nSession ends at {now()}\n-----------------------\n")
 
@@ -65,38 +74,49 @@ def sendnovar(database:str, body:str, subject:str, email_field:str, credentials:
 
     session.quit()
 
-def sendvar(database:str, template:str, subject:str, email_field:str, variables:dict, credentials:dict, type:str):
-    #loading database
-    database = open(database, 'r', encoding='utf-8')
+
+def sendvar(
+    database: str,
+    template: str,
+    subject: str,
+    email_field: str,
+    variables: dict,
+    credentials: dict,
+    type: str,
+):
+    # loading database
+    database = open(database, "r", encoding="utf-8")
     reader = DictReader(database)
 
-    #start session
+    # start session
     session = startsession(credentials)
     SENDER_EMAIL = credentials.get("SENDER_EMAIL")
 
-    #making logfiles
-    successlog = open('success.log', 'a', encoding='utf-8')
-    failurelog = open('failure.log', 'a', encoding='utf-8')
+    # making logfiles
+    successlog = open("success.log", "a", encoding="utf-8")
+    failurelog = open("failure.log", "a", encoding="utf-8")
     successlog.write(f"\nSession starts at {now()}\n")
     failurelog.write(f"\nSession starts at {now()}\n")
-    
-    #email processing
+
+    # email processing
     for row in reader:
         RECEIVER_EMAIL = row[email_field]
         try:
             email = MIMEMultipart()
-            email['From'] = SENDER_EMAIL
-            email['To'] = RECEIVER_EMAIL
-            #subject = replace_var(subject, variables, row)
-            email['Subject'] = subject
+            email["From"] = SENDER_EMAIL
+            email["To"] = RECEIVER_EMAIL
+            # subject = replace_var(subject, variables, row)
+            email["Subject"] = subject
             body = replace_var(template, variables, row)
             email.attach(MIMEText(body, type))
             session.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, email.as_string())
             successlog.write(f"Email sent to {RECEIVER_EMAIL} at {now()}\n")
         except:
-            failurelog.write(f"Failed to send email to {RECEIVER_EMAIL}. Time: {now()}\n")
-    
-    #ending session and closing logs
+            failurelog.write(
+                f"Failed to send email to {RECEIVER_EMAIL}. Time: {now()}\n"
+            )
+
+    # ending session and closing logs
     successlog.write(f"\nSession ends at {now()}\n-----------------------\n")
     failurelog.write(f"\nSession ends at {now()}\n-----------------------\n")
 
