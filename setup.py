dab518420fdd9b606ce42c4ad52c5483b21bba9d
roughweed <cavnedi@gmail.com>
2020-12-26 14:20:18 +0600

    version 1.0.0

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages 
+  
+with open('requirements.txt') as f: 
+    requirements = f.readlines() 
+  
+long_description = open('README.md', 'r', encoding='utf-8').read() 
+  
+setup( 
+        name ='bulkmail', 
+        version ='1.0.0', 
+        author ='Rafeed M. Bhuiyan', 
+        author_email ='rafeedm.bhuiyan@gmail.com', 
+        url ='https://github.com/roughweed/csv-bulk-email', 
+        description ='Python package for sending bulk emails.', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT', 
+        packages = find_packages(), 
+        entry_points ={ 
+            'console_scripts': [ 
+                'bulkmail = bulkmail.commandline:main'
+            ] 
+        },
+        classifiers =( 
+            "Programming Language :: Python :: 3", 
+            "License :: OSI Approved :: MIT License", 
+            "Operating System :: OS Independent", 
+        ), 
+        keywords ='email bulk cli mail', 
+        install_requires = requirements, 
+        zip_safe = False
+) 
